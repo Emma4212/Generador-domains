f1dcc30f5aac53a60404cb9f6ccf8665141b2e54
Andrés Arreaza <112995166+Andres-Arreaza@users.noreply.github.com>
2024-09-03 00:57:39 -0400

    CORRECCION

--- src/app.js
+++ src/app.js
@@ -3,19 +3,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-
-
 // Define los arrays pronoun, adj y noun
 let pronoun = ["the", "sis"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-
-
+// Creacion de la función se ejecuta cuando la página web ha terminado de cargarse-Andres-Arreaza.
 window.onload = function() {
   const list = document.getElementById("list");
   let domains = [];
-  
+  // Creacion de los for anidados para recorrer los arrays-Andres-Arreaza.
   for (let i = 0; i < pronoun.length; i++) {
     for (let a = 0; a < adj.length; a++) {
       for (let b = 0; b < noun.length; b++) {
